2285b74050ee5235e8d4a9c7bd3c488255293a46
ypafeuer <poi180626@outlook.com>
2018-07-25 09:30:54 +0800

    a

---
+++ JavaApp180725/src/javaapp180725/JavaApp.java
@@ -0,0 +1,12 @@
+
+package javaapp180725;
+
+public class JavaApp {
+
+    public static void main(String[] args) {
+        int result=30-12/(2*5)+1;
+        System.out.println(""+result);
+            
+    }
+    
+}
