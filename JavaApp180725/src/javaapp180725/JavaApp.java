dd54218a52cd3463661e82bee577d6137b793621
ypafeuer <poi180626@outlook.com>
2018-07-25 09:59:33 +0800

    Signed-off-by: ypafeuer <poi180626@outlook.com>

--- JavaApp180725/src/javaapp180725/JavaApp.java
+++ JavaApp180725/src/javaapp180725/JavaApp.java
@@ -2,11 +2,20 @@
 package javaapp180725;
 
 public class JavaApp {
-
+    private int x=0;
+    private int y=0;
     public static void main(String[] args) {
-        int result=30-12/(2*5)+1;
-        System.out.println(""+result);
-            
+        JavaApp app=new JavaApp();
+        app.printthis(1, 2);
+        app.printthat(3, 4);
+    }
+    public void printthis(int x, int y){
+        x=x;y=y;
+        System.out.println(""+x+y);
+    }
+    public void printthat(int x, int y){
+        this.x=x;this.y=y;
+        System.out.println(""+x+y);
     }
     
 }
