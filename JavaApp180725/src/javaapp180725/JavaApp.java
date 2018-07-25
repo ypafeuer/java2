0d94719fa2bcc7c7ba4e8ffffb8f07cc138d4817
ypafeuer <poi180626@outlook.com>
2018-07-25 12:12:44 +0800

    000

--- JavaApp180725/src/javaapp180725/JavaApp.java
+++ JavaApp180725/src/javaapp180725/JavaApp.java
@@ -2,20 +2,20 @@
 package javaapp180725;
 
 public class JavaApp {
-    private int x=0;
-    private int y=0;
+
     public static void main(String[] args) {
-        JavaApp app=new JavaApp();
-        app.printthis(1, 2);
-        app.printthat(3, 4);
+String[] t={"a","b","c"};
+int ii=0;
+for(String aString:t){
+    while(ii<t.length){
+            System.out.println(ii);
+            ii++;
+            break;
     }
-    public void printthis(int x, int y){
-        x=x;y=y;
-        System.out.println(""+this.x+this.y);
-    }
-    public void printthat(int x, int y){
-        this.x=x;this.y=y;
-        System.out.println(""+this.x+this.y);
-    }//
+}
+        
+        }
+
+
     
 }
